aebe49c8d7772ed3cc2094e40388dd9d2afe0844
Fernando <luisfernandosalasg@gmail.com>
2022-05-10 20:54:34 -0500

    Add Missing number

---
+++ 3. Pattern - Cyclic Sort/missing number.js
@@ -0,0 +1,23 @@
+function find_missing_number(nums) {
+	let i = 0;
+	const n = nums.length;
+	while (i < n) {
+		j = nums[i];
+		if (nums[i] < n && nums[i] !== nums[j]) {
+			[nums[i], nums[j]] = [nums[j], nums[i]]; // swap
+		} else {
+			i += 1;
+		}
+	}
+
+	// find the first number missing from its index, that will be our required number
+	for (i = 0; i < n; i++) {
+		if (nums[i] !== i) {
+			return i;
+		}
+	}
+
+	return n;
+}
+
+console.log(find_missing_number([4, 0, 3, 1]));
