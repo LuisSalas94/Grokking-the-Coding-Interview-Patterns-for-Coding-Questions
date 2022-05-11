4825e961773fd7e926696f43f44bd1f7e14aeb8a
Fernando Salas <luisfernandosalasg@gmail.com>
2022-05-11 16:22:15 -0500

    Add Missing numbers

---
+++ 3. Pattern - Cyclic Sort/missing_numbers.js
@@ -0,0 +1,24 @@
+function find_missing_numbers(nums) {
+	let i = 0;
+	while (i < nums.length) {
+		const j = nums[i] - 1;
+		if (nums[i] !== nums[j]) {
+			//swap
+			[nums[i], nums[j]] = [nums[j], nums[i]];
+		} else {
+			i += 1;
+		}
+	}
+
+	let missingNumbers = [];
+
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] !== i + 1) {
+			missingNumbers.push(i + 1);
+		}
+	}
+
+	return missingNumbers;
+}
+
+console.log(find_missing_numbers([2, 3, 1, 8, 2, 3, 5, 1]));
